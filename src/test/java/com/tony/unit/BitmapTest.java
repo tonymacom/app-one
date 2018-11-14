9dabf2a2ee8ed299bab5f51e1adf339687500e4f
tony-Ma-yami <tony.ma@yamibuy.com>
2018-11-14 16:15:35 +0800

    update

---
+++ src/test/java/com/tony/unit/BitmapTest.java
@@ -0,0 +1,27 @@
+package com.tony.unit;
+
+import org.junit.Test;
+
+import java.util.BitSet;
+
+/**
+ * @author www.yamibuy.com
+ * @desc :
+ * @date 2018/11/13
+ * <b>版权所有：</b>版权所有(C) 2018，www.yamibuy.com<br>
+ */
+public class BitmapTest {
+
+
+
+
+    @Test
+    public void test(){
+        BitSet bitSet = new BitSet();
+
+        bitSet.set(1);
+
+
+    }
+
+}
